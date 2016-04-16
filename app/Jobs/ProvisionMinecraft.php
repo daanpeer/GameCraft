b7c08568a8ceda2deb0867d0b2bcadbb0ceb3164
Daan Houweling <daan.gdh@gmail.com>
2016-04-16 14:55:01 +0200

    Added logging

--- app/Jobs/ProvisionMinecraft.php
+++ app/Jobs/ProvisionMinecraft.php
@@ -24,12 +24,12 @@ class ProvisionMinecraft
 
     public function handle()
     {
-        echo "PROVISIONING MINECRAFT ON " . $this->server->name;
+        echo "PROVISIONING MINECRAFT ON " . $this->server->ip . PHP_EOL;
 
         $cd = 'cd ' . app_path() . '/provisioning/minecraft/';
         shell_exec($cd . ' && envoy run minecraft_provision --host=root@' . $this->server->ip);
 
-        echo "STARTING MINECRAFT ON " . $this->server->name;
+        echo "STARTING MINECRAFT ON " . $this->server->name . PHP_EOL;
         shell_exec($cd . ' && envoy run minecraft_start --host=root@' . $this->server->ip);
 
         $this->server->status = Server::RUNNING;
