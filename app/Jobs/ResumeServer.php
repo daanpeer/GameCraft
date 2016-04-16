e478afca68c0977b2af20e37a3e55505b4350a9c
Luuk Holleman <luukholleman@gmail.com>
2016-04-16 16:59:10 +0200

    dsglds

--- app/Jobs/ResumeServer.php
+++ app/Jobs/ResumeServer.php
@@ -21,7 +21,10 @@ class ResumeServer extends Job
 
     public function handle()
     {
-        $do = \DigitalOcean::droplet()->create($this->server->name, 'ams3', '2gb', $this->server->snapshot);
+        $do = \DigitalOcean::droplet()->create($this->server->name, 'ams3', '2gb', $this->server->snapshot, false, false, false, [
+            "05:95:d5:ad:01:d8:b3:f8:8b:56:97:8e:37:32:3c:3d",
+            "c7:a3:d0:95:0e:f6:53:5b:95:db:b5:2a:fd:14:d7:63"
+        ]);
 
         $this->server->do_id = $do->id;
         $this->server->status = Server::RESUMING;
