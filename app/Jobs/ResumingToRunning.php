eece3883bba48d04ce0b23cd43b6d49d65b17878
Daan Houweling <daan.gdh@gmail.com>
2016-04-16 15:07:19 +0200

    Resuming games

--- app/Jobs/ResumingToRunning.php
+++ app/Jobs/ResumingToRunning.php
@@ -16,6 +16,19 @@ class ResumingToRunning extends Job
             $do = \DigitalOcean::droplet()->getById($server->do_id);
             if($do->status == "active") {
                 echo "STARTING SERVER AGAIN";
+                switch ($server->game) {
+                    case Server::GAME_FACTORIO:
+                        echo "STARTING FACTORIO ON " . $server->name . PHP_EOL;
+                        $cd = 'cd ' . app_path() . '/provisioning/factorio/';
+                        shell_exec($cd . ' && envoy run factorio_start --host=root@' . $server->ip);
+                        break;
+
+                    case Server::GAME_MINECRAFT:
+                        echo "STARTING MINECRAFT ON " . $server->name . PHP_EOL;
+                        $cd = 'cd ' . app_path() . '/provisioning/minecraft/';
+                        shell_exec($cd . ' && envoy run minecraft_start --host=root@' . $server->ip);
+                        break;
+                }
 
                 $server->status = Server::RUNNING;
                 $server->save();
